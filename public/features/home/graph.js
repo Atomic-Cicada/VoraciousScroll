ed142ade1a56921fa1ba5f9a6b4d76b9612b9368
Aljosha Novakovic <ollynov@gmail.com>
2016-09-13 17:07:30 -0700

    Have barebones homepage graph files and structure

---
+++ public/features/home/graph.js
@@ -0,0 +1,12 @@
+angular.module('smartNews.home')
+
+.controller('GraphCtrl', function($scope, TopTrendsFactory, saveArticle, isAuth) {
+
+  // $scope.graph = GraphFactory.currentGraph;
+
+  $scope.isAuth = function() {
+    $scope.user = isAuth();
+    return !!isAuth();
+  };
+
+});
