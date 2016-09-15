e4969651dc28d608e107a26e086f0bba8938adec
Aljosha Novakovic <ollynov@gmail.com>
2016-09-15 12:27:29 -0700

    BareBones helloWorld sendgrid email setup but no test yet

---
+++ server/mails/helloWorld.js
@@ -0,0 +1,19 @@
+var helper = require('sendgrid').mail;
+var fromEmail = new helper.Email('ollynov@gmail.com');
+var toEmail = new helper.Email('ollynov@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
