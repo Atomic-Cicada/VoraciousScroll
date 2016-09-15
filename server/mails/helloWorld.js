1e82d0e0f78a33b0d30503c68f5a0a73e2a33139
Aljosha Novakovic <ollynov@gmail.com>
2016-09-15 13:55:11 -0700

    Implementing correct sendgrid api keys

--- server/mails/helloWorld.js
+++ server/mails/helloWorld.js
@@ -1,11 +1,12 @@
 var helper = require('sendgrid').mail;
 var fromEmail = new helper.Email('ollynov@gmail.com');
 var toEmail = new helper.Email('ollynov@gmail.com');
-var subject = 'Hello World from Yosh and the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'Hello, Email!');
+var subject = 'FROM YOSH AGAIN';
+var content = new helper.Content('text/plain', 'Hello, there!');
 var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+var sendGridKeys = require('../../keys.js').sendgrid;
 // process.env.SENDGRID_API_KEY
-var sg = require('sendgrid')('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');
+var sg = require('sendgrid')(sendGridKeys.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
