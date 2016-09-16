b6a3a522efd35a045ae8e1c23a19b72c8d4cac5e
Andrew <andrewk17@gmail.com>
2016-09-15 20:53:20 -0700

    add key examples back

---
+++ keys.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+ facebook: {
+   FACEBOOK_APP_ID: 177740742652029,
+   FACEBOOK_APP_SECRET: 'c25c713608453d55b2196bfc3012c235'
+ },
+ aylien: {
+   app_id: '69c7647b',
+   app_key: 'd6c6cd366b4ca75beeb6c1da3bb04ada'
+ }
+};
